dd8603deedd2137300251edb108541098647db86
zormax <zormax052@gmail.com>
2022-03-29 02:12:36 +0300

    eslint env added

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,53 +1,44 @@
 module.exports = {
-  "root": true,
-  "ignorePatterns": [
-    "projects/**/*"
-  ],
-  "overrides": [
+  root: true,
+  ignorePatterns: ['projects/**/*'],
+  overrides: [
     {
-      "files": [
-        "*.ts"
-      ],
-      "parserOptions": {
-        "tsconfigRootDir": __dirname,
-        "project": [
-          "tsconfig.json"
-        ],
-        "createDefaultProgram": true
+      files: ['*.ts'],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['tsconfig.json'],
+        createDefaultProgram: true,
       },
-      "extends": [
-        "plugin:@angular-eslint/recommended",
-        "plugin:@angular-eslint/template/process-inline-templates",
-        "plugin:@typescript-eslint/recommended",
-        "eslint:recommended"
+      env: { browser: true },
+      extends: [
+        'plugin:@angular-eslint/recommended',
+        'plugin:@angular-eslint/template/process-inline-templates',
+        'plugin:@typescript-eslint/recommended',
+        'eslint:recommended',
       ],
-      "rules": {
-        "@angular-eslint/directive-selector": [
-          "error",
+      rules: {
+        '@angular-eslint/directive-selector': [
+          'error',
           {
-            "type": "attribute",
-            "prefix": "app",
-            "style": "camelCase"
-          }
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase',
+          },
         ],
-        "@angular-eslint/component-selector": [
-          "error",
+        '@angular-eslint/component-selector': [
+          'error',
           {
-            "type": "element",
-            "prefix": "app",
-            "style": "kebab-case"
-          }
-        ]
-      }
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case',
+          },
+        ],
+      },
     },
     {
-      "files": [
-        "*.html"
-      ],
-      "extends": [
-        "plugin:@angular-eslint/template/recommended"
-      ],
-      "rules": {}
-    }
-  ]
-}
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {},
+    },
+  ],
+};
