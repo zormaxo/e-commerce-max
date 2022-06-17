ce306fc5b966563cd07c9bbaddf7229b842e6592
zormax <zormax052@gmail.com>
2022-06-18 00:04:24 +0300

    namespace fix, solution does not compiled

--- Core/Dtos/LoginDto.cs
+++ Core/Dtos/LoginDto.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace API.DTOs
+namespace Core.DTOs
 {
     public class LoginDto
     {
--- Core/Dtos/UserDto.cs
+++ Core/Dtos/UserDto.cs
@@ -1,4 +1,4 @@
-namespace API.DTOs
+namespace Core.DTOs
 {
     public class UserDto
     {
