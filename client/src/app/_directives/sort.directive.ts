047b3f86cdd7fbef33968638a1356ec9bd01595e
zormax <zormax052@gmail.com>
2022-07-11 11:11:53 +0300

    arrows full working

--- client/src/app/_directives/sort.directive.ts
+++ client/src/app/_directives/sort.directive.ts
@@ -26,6 +26,7 @@ export class SortDirective {
     this.up = this.renderer.createElement('em');
     this.renderer.addClass(this.up, 'fa-solid');
     this.renderer.addClass(this.up, 'fa-caret-up');
+    this.renderer.addClass(this.up, 'sortem');
 
     this.omer = this.renderer.createElement('span');
     this.omer.innerText = 'omer';
@@ -38,30 +39,33 @@ export class SortDirective {
   }
 
   @HostListener('click') click() {
-    let tableElement = this.elRef.nativeElement.parentNode.parentNode.parentNode;
-    if (tableElement instanceof HTMLTableElement) {
-      var omer = tableElement.getElementsByTagName('th');
-      var salih = omer[2];
-      this.renderer.removeChild(salih, this.span);
-      this.renderer.removeChild(salih, this.salih);
-      this.renderer.removeChild(salih, this.asli);
-    }
+  
+      let tableElement: HTMLElement = this.elRef.nativeElement.parentNode.parentNode.parentNode;
+      // if (tableElement instanceof HTMLTableElement) {
+      //   var omer = tableElement.getElementsByTagName('th');
+      //   var salih = omer[2];
+      //   var child = omer[2].lastChild;
+      //   this.renderer.removeChild('salih', child);
+      //   this.renderer.removeChild(salih, this.salih);
+      //   this.renderer.removeChild(salih, this.asli);
+      // }
+
+      var elements = tableElement.getElementsByClassName('sortem');
+      Array.from(elements).forEach((el) => {
+        this.renderer.removeChild(el.parentNode, el);
+      });
+  
     if (this.arrow == 'none') {
-      this.renderer.appendChild(this.elRef.nativeElement, this.omer);
-      // this.renderer.appendChild(this.elRef.nativeElement, this.space);
-      // this.renderer.appendChild(this.elRef.nativeElement, this.down);
+      this.renderer.appendChild(this.elRef.nativeElement, this.space);
+      this.renderer.appendChild(this.elRef.nativeElement, this.down);
       this.arrow = 'down';
     } else if (this.arrow == 'down') {
-      this.renderer.appendChild(this.elRef.nativeElement, this.salih);
-      this.renderer.removeChild(this.elRef.nativeElement, this.asli);
-      // this.renderer.removeChild(this.elRef.nativeElement, this.down);
-      // this.renderer.appendChild(this.elRef.nativeElement, this.up);
+      this.renderer.removeChild(this.elRef.nativeElement, this.down);
+      this.renderer.appendChild(this.elRef.nativeElement, this.up);
       this.arrow = 'up';
     } else if (this.arrow == 'up') {
-      // this.renderer.removeChild(this.elRef.nativeElement, this.up);
-      // this.renderer.appendChild(this.elRef.nativeElement, this.down);
-      this.renderer.removeChild(this.elRef.nativeElement, this.salih);
-      this.renderer.appendChild(this.elRef.nativeElement, this.asli);
+      this.renderer.removeChild(this.elRef.nativeElement, this.up);
+      this.renderer.appendChild(this.elRef.nativeElement, this.down);
       this.arrow = 'down';
     }
 
