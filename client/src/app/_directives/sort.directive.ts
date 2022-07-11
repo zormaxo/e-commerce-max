3350210a4964b2a058169fe2059a01a2875eabda
zormax <zormax052@gmail.com>
2022-07-11 11:37:01 +0300

    sort directive finish till events

--- client/src/app/_directives/sort.directive.ts
+++ client/src/app/_directives/sort.directive.ts
@@ -1,83 +1,73 @@
-import { Directive, ElementRef, EventEmitter, HostListener, Output, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, OnInit, Output, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSort]',
 })
-export class SortDirective {
+export class SortDirective implements OnInit {
   @Output() someEvent: EventEmitter<string> = new EventEmitter();
-  arrow = 'none';
+  arrow: Direction = Direction.None;
   down: unknown;
   up: unknown;
   space: { innerText: string };
-  omer;
-  salih;
-  asli;
+  thText: ChildNode;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
-    this.renderer.addClass(this.elRef.nativeElement, 'text-primary');
-
-    this.space = this.renderer.createElement('span');
-    this.space.innerText = ' ';
-
-    this.down = this.renderer.createElement('em');
-    this.renderer.addClass(this.down, 'fa-solid');
-    this.renderer.addClass(this.down, 'fa-caret-down');
-
-    this.up = this.renderer.createElement('em');
-    this.renderer.addClass(this.up, 'fa-solid');
-    this.renderer.addClass(this.up, 'fa-caret-up');
-    this.renderer.addClass(this.up, 'sortem');
-
-    this.omer = this.renderer.createElement('span');
-    this.omer.innerText = 'omer';
-
-    this.salih = this.renderer.createElement('span');
-    this.salih.innerText = 'salih';
-
-    this.asli = this.renderer.createElement('span');
-    this.asli.innerText = 'asli';
+    this.CreateElements();
+  }
+  ngOnInit(): void {
+    this.thText = (<HTMLElement>this.elRef.nativeElement).firstChild;
+    this.renderer.addClass(this.thText, 'text-primary');
   }
 
   @HostListener('click') click() {
-  
-      let tableElement: HTMLElement = this.elRef.nativeElement.parentNode.parentNode.parentNode;
-      // if (tableElement instanceof HTMLTableElement) {
-      //   var omer = tableElement.getElementsByTagName('th');
-      //   var salih = omer[2];
-      //   var child = omer[2].lastChild;
-      //   this.renderer.removeChild('salih', child);
-      //   this.renderer.removeChild(salih, this.salih);
-      //   this.renderer.removeChild(salih, this.asli);
-      // }
+    const tableElement: HTMLTableElement = this.elRef.nativeElement.parentNode.parentNode.parentNode;
+    const elements = tableElement.getElementsByClassName('sortem');
+    Array.from(elements).forEach((el) => {
+      this.renderer.removeChild(el.parentNode, el);
+    });
 
-      var elements = tableElement.getElementsByClassName('sortem');
-      Array.from(elements).forEach((el) => {
-        this.renderer.removeChild(el.parentNode, el);
-      });
-  
-    if (this.arrow == 'none') {
+    if (this.arrow == Direction.None) {
       this.renderer.appendChild(this.elRef.nativeElement, this.space);
       this.renderer.appendChild(this.elRef.nativeElement, this.down);
-      this.arrow = 'down';
-    } else if (this.arrow == 'down') {
-      this.renderer.removeChild(this.elRef.nativeElement, this.down);
+      this.arrow = Direction.Down;
+    } else if (this.arrow == Direction.Down) {
       this.renderer.appendChild(this.elRef.nativeElement, this.up);
-      this.arrow = 'up';
-    } else if (this.arrow == 'up') {
-      this.renderer.removeChild(this.elRef.nativeElement, this.up);
+      this.arrow = Direction.Up;
+    } else if (this.arrow == Direction.Up) {
       this.renderer.appendChild(this.elRef.nativeElement, this.down);
-      this.arrow = 'down';
+      this.arrow = Direction.Down;
     }
 
     this.someEvent.emit('omer');
   }
 
   @HostListener('mouseenter') mouseenter() {
-    this.renderer.addClass(this.elRef.nativeElement, 'text-decoration-underline');
-    this.renderer.setStyle(this.elRef.nativeElement, 'cursor', 'pointer');
+    this.renderer.addClass(this.thText, 'text-decoration-underline');
+    this.renderer.setStyle(this.thText, 'cursor', 'pointer');
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.renderer.removeClass(this.elRef.nativeElement, 'text-decoration-underline');
+    this.renderer.removeClass(this.thText, 'text-decoration-underline');
   }
+
+  private CreateElements() {
+    this.space = this.renderer.createElement('span');
+    this.space.innerText = ' ';
+
+    this.down = this.renderer.createElement('em');
+    this.renderer.addClass(this.down, 'fa-solid');
+    this.renderer.addClass(this.down, 'fa-caret-down');
+    this.renderer.addClass(this.down, 'sortem');
+
+    this.up = this.renderer.createElement('em');
+    this.renderer.addClass(this.up, 'fa-solid');
+    this.renderer.addClass(this.up, 'fa-caret-up');
+    this.renderer.addClass(this.up, 'sortem');
+  }
+}
+
+enum Direction {
+  Up = 1,
+  Down,
+  None,
 }
