60844e635a6fb85242a5e9b1e5e20c8b35ef87e3
zormax <zormax052@gmail.com>
2022-07-11 10:33:22 +0300

    .

--- client/src/app/_directives/sort.directive.ts
+++ client/src/app/_directives/sort.directive.ts
@@ -8,7 +8,10 @@ export class SortDirective {
   arrow = 'none';
   down: unknown;
   up: unknown;
-  space: { innerText: string; };
+  space: { innerText: string };
+  omer;
+  salih;
+  asli;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
     this.renderer.addClass(this.elRef.nativeElement, 'text-primary');
@@ -23,20 +26,42 @@ export class SortDirective {
     this.up = this.renderer.createElement('em');
     this.renderer.addClass(this.up, 'fa-solid');
     this.renderer.addClass(this.up, 'fa-caret-up');
+
+    this.omer = this.renderer.createElement('span');
+    this.omer.innerText = 'omer';
+
+    this.salih = this.renderer.createElement('span');
+    this.salih.innerText = 'salih';
+
+    this.asli = this.renderer.createElement('span');
+    this.asli.innerText = 'asli';
   }
 
   @HostListener('click') click() {
+    let tableElement = this.elRef.nativeElement.parentNode.parentNode.parentNode;
+    if (tableElement instanceof HTMLTableElement) {
+      var omer = tableElement.getElementsByTagName('th');
+      var salih = omer[2];
+      this.renderer.removeChild(salih, this.span);
+      this.renderer.removeChild(salih, this.salih);
+      this.renderer.removeChild(salih, this.asli);
+    }
     if (this.arrow == 'none') {
-      this.renderer.appendChild(this.elRef.nativeElement, this.space);
-      this.renderer.appendChild(this.elRef.nativeElement, this.down);
+      this.renderer.appendChild(this.elRef.nativeElement, this.omer);
+      // this.renderer.appendChild(this.elRef.nativeElement, this.space);
+      // this.renderer.appendChild(this.elRef.nativeElement, this.down);
       this.arrow = 'down';
     } else if (this.arrow == 'down') {
-      this.renderer.removeChild(this.elRef.nativeElement, this.down);
-      this.renderer.appendChild(this.elRef.nativeElement, this.up);
+      this.renderer.appendChild(this.elRef.nativeElement, this.salih);
+      this.renderer.removeChild(this.elRef.nativeElement, this.asli);
+      // this.renderer.removeChild(this.elRef.nativeElement, this.down);
+      // this.renderer.appendChild(this.elRef.nativeElement, this.up);
       this.arrow = 'up';
     } else if (this.arrow == 'up') {
-      this.renderer.removeChild(this.elRef.nativeElement, this.up);
-      this.renderer.appendChild(this.elRef.nativeElement, this.down);
+      // this.renderer.removeChild(this.elRef.nativeElement, this.up);
+      // this.renderer.appendChild(this.elRef.nativeElement, this.down);
+      this.renderer.removeChild(this.elRef.nativeElement, this.salih);
+      this.renderer.appendChild(this.elRef.nativeElement, this.asli);
       this.arrow = 'down';
     }
 
