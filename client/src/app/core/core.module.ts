25580b81a546eeac3d34b67bdfb5cd522c4dba6b
zormax <zormax052@gmail.com>
2022-07-14 00:52:34 +0300

    membership info

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,10 +4,17 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { RouterModule } from '@angular/router';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   declarations: [NavBarComponent],
-  imports: [CommonModule, BrowserAnimationsModule, BsDropdownModule.forRoot(), RouterModule],
-  exports: [NavBarComponent, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+  ],
+  exports: [NavBarComponent, RouterModule, BsDatepickerModule],
 })
 export class CoreModule {}
