e2708c1ddba075dea5f9a5bf6b448b93b60244fa
zormax <zormax052@gmail.com>
2022-09-16 22:54:47 +0300

    folder rearranged

---
+++ Services/Catalog/API/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,22 @@
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" }));
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            //// app.UseDeveloperExceptionPage(); //we don't use anymore
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            return app;
+        }
+    }
+}
