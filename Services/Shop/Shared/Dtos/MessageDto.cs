1de3db52f36e1bf713b09936fc2ba71144a2bc3e
zormax <okocadere@gmail.com>
2023-02-06 17:43:34 +0300

    dto

--- Services/Shop/Shared/Dtos/MessageDto.cs
+++ Services/Shop/Shared/Dtos/MessageDto.cs
@@ -8,21 +8,21 @@ public class MessageDto
 
     public int SenderId { get; set; }
 
-    public string SenderUsername { get; set; }
+    public string SenderUsername { get; set; } = string.Empty;
 
-    public string SenderPhotoUrl { get; set; }
+    public string SenderPhotoUrl { get; set; } = string.Empty;
 
-    public MemberDto Sender { get; set; }
+    public MemberDto? Sender { get; set; }
 
-    public MemberDto Recipient { get; set; }
+    public MemberDto? Recipient { get; set; }
 
     public int RecipientId { get; set; }
 
-    public string RecipientUsername { get; set; }
+    public string RecipientUsername { get; set; } = string.Empty;
 
-    public string RecipientPhotoUrl { get; set; }
+    public string RecipientPhotoUrl { get; set; } = string.Empty;
 
-    public string Content { get; set; }
+    public string Content { get; set; } = string.Empty;
 
     public DateTime? DateRead { get; set; }
 
